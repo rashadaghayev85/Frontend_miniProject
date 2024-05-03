66cfa20379a70ff3f765d24880da8a7e60762221
rashadaghayev85 <rashadra@code.edu.az>
2024-05-03 17:48:08 +0400

    Some problems solved

--- project/assets/js/home.js
+++ project/assets/js/home.js
@@ -1,1 +1,16 @@
 "use strict";
+
+
+document.addEventListener('scroll' , () => {
+    let header = document.querySelector ("header")
+    let body = document.querySelector ('body')
+    
+    if (window.scrollY > 0) {
+        header.classList.add('scroll')
+        // body.style.padding = "90px 0 0 0"
+    }
+    else {
+        header.classList.remove('scroll')
+        // body.style.padding = "0"
+    }
+})
