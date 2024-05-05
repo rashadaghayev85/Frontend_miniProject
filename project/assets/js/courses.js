9086062a7353233efe09da6f7184f6fa72d9714e
rashadaghayev85 <rashadra@code.edu.az>
2024-05-06 01:37:29 +0400

    blog and contact pages fixed

---
+++ project/assets/js/courses.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+document.addEventListener('scroll' , () => {
+    let header = document.querySelector ("header")
+    let body = document.querySelector ('body')
+    let logo = document.querySelector ('.dropdown, .pages > ul > li > a ')
+    let link = document.querySelectorAll ('.nav-link')
+    
+    if (window.scrollY > 0) {
+        header.classList.add('scroll')
+        logo.style.color = "#000"
+        //link.style.color = "#000"
+        link.forEach(element => {
+            element.style.color="#000"
+        });
+        // body.style.padding = "90px 0 0 0"
+    }
+    else {
+        header.classList.remove('scroll')
+        logo.style.color = "#fff"
+        link.forEach(element => {
+            element.style.color="#fff"
+        });
+        // body.style.padding = "0"
+    }
+})
