e6b65e912f915ac6a6b5cc408a080875a2da671d
rashadaghayev85 <rashadra@code.edu.az>
2024-05-07 12:49:31 +0400

    some problems fixed

--- project/assets/js/blog.js
+++ project/assets/js/blog.js
@@ -25,3 +25,15 @@ document.addEventListener('scroll' , () => {
         // body.style.padding = "0"
     }
 })
+let input=document.querySelector(".recent-post input");
+
+input.addEventListener("click",function(){
+  
+    input.parentElement.style.border="1px solid blue "
+
+})
+input.addEventListener("blur",function(){
+  
+    input.parentElement.style.border="none"
+
+})
